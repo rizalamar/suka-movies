6a7ed75bf72cce9fe1a931256733a4197b119d46
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-05-16 22:13:38 +0700

    restyling movies by genre

--- src/components/movie/MovieCard.tsx
+++ src/components/movie/MovieCard.tsx
@@ -16,38 +16,50 @@ const MovieCard = ({ item, onPress }: Props) => {
 	return (
 		<TouchableOpacity
 			onPress={onPress}
-			style={{ width: CARD_WIDTH }}
-			className="bg-surface w-[48%] mb-4 rounded-xl overflow-hidden border border-slate-800 active:opacity-90"
+			style={{ width: CARD_WIDTH, overflow: "hidden" }}
+			className="mb-4 rounded-2xl active:opacity-90"
 		>
-			{item.poster_path ? (
-				<RNImage
-					source={{ uri: `${IMAGE_BASE_URL}${item.poster_path}` }}
-					contentFit="cover"
-					style={{
-						width: "100%",
-						aspectRatio: 2 / 3,
-					}}
-					transition={300}
-				/>
-			) : (
-				<View className="items-center justify-center flex-1">
-					<Text className="text-sm text-textSecondary">No Image</Text>
-				</View>
-			)}
+			<View className="relative">
+				{item.poster_path ? (
+					<RNImage
+						source={{ uri: `${IMAGE_BASE_URL}${item.poster_path}` }}
+						contentFit="cover"
+						style={{
+							width: "100%",
+							aspectRatio: 2 / 3,
+							borderRadius: 8,
+						}}
+						transition={300}
+					/>
+				) : (
+					<View style={{ width: "100%", aspectRatio: 2 / 3 }} className="items-center justify-center">
+						<FontAwesome name="file-image-o" size={24} color="#475569" />
+						<Text className="mt-2 text-lg text-slate-500">No Poster</Text>
+					</View>
+				)}
 
-			<View className="p-3">
-				<Text className="font-bold text-textPrimary" numberOfLines={1}>
+				{/* Rating */}
+				<View
+					className="absolute flex-row items-center gap-2 border border-slate-700/50"
+					style={{
+						backgroundColor: "#1e293b",
+						paddingBlock: 4,
+						paddingInline: 6,
+						top: 8,
+						left: 8,
+						borderRadius: 8,
+					}}
+				>
+					<FontAwesome name="star" size={18} color="#f59e0b" />
+					<Text className="text-lg font-bold text-accent">{item.vote_average.toFixed(1)}</Text>
+				</View>
+			</View>
+
+			{/* Title  */}
+			<View className="items-center p-3">
+				<Text className="text-xl font-bold tracking-wider text-center text-textPrimary" numberOfLines={2}>
 					{item.title}
 				</Text>
-				<View className="flex-row items-center mt-2">
-					<View className="flex-row items-center gap-2">
-						<FontAwesome name="star" size={18} color="#f59e0b" />
-						<Text className="text-sm text-accent">{item.vote_average.toFixed(1)}</Text>
-					</View>
-
-					<View className="w-1 h-1 mx-2 rounded-full bg-slate-800" />
-					<Text className="text-sm text-textSecondary">{item.release_date}</Text>
-				</View>
 			</View>
 		</TouchableOpacity>
 	);
